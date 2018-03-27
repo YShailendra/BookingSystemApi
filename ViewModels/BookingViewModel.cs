12887760f3abc1c8f1d62a2a365b5ec70372f183
@ershailendra57 <ershailendra57@gmail.com>
2018-03-27 13:09:18 +0530

    code updated for booking validation

--- ViewModels/BookingViewModel.cs
+++ ViewModels/BookingViewModel.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using BookingSystemApi.Models;
 using BookingSystemApi.Repository;
 using Newtonsoft.Json;
-
+using BookingSystemApi.Helper;
 namespace BookingSystemApi.ViewModels
 {
     public class BookingViewModel:BaseViewModel
@@ -23,32 +23,46 @@ namespace BookingSystemApi.ViewModels
         }
         public ClientMessage<BookingModel> SaveBookingDetails(BookingModel model)
         {
-           model.BookingNumber= base.GenerateTicketNumber();
-           Console.WriteLine(model.BookingNumber);
-           model.CreatedDate=DateTime.Now;
-           model.ID=Guid.NewGuid();
-           //model.BusID=Guid.NewGuid();
-           var data = _bookingRepo.Add(model);
-           var _clientMessage= new ClientMessage<BookingModel>();
-           _clientMessage.ClientData=model;
-           if(data.IsCompletedSuccessfully)
-           {
-               _clientMessage.HasError=true;
-           }
-           
-           return _clientMessage;
+            var _clientMessage = new ClientMessage<BookingModel>();
+            if (this.ValidateBooking(model))
+            {
+                model.BookingNumber = base.GenerateTicketNumber();
+                Console.WriteLine(model.BookingNumber);
+                model.CreatedDate = DateTime.Now;
+                model.ID = Guid.NewGuid();
+                var data = _bookingRepo.Add(model);
+
+                _clientMessage.ClientData = model;
+                //this.SendMailToTicketOwner(model);
+                if (data.IsCompletedSuccessfully)
+                {
+                    _clientMessage.HasError = true;
+                }
+            }
+            else{
+                 _clientMessage.ClientData = model;
+                  _clientMessage.HasError = true;
+                   _clientMessage.Message = "Seat Already Booked!";
+            }
+
+
+            return _clientMessage;
         }
         public async Task<List<SeatDetails>> GetBookedSeats(BookingModel model)
         {
-           
-           var data = await _bookingRepo.GetBookedTicketDetails(model);
-           var result= new List<SeatDetails>();
-           foreach (var item in data)
-           {
-              result.AddRange(JsonConvert.DeserializeObject<List<SeatDetails>>(item));
-           }  
-           return result;
-           
+
+            var data = await _bookingRepo.GetBookedTicketDetails(model);
+            var result = new List<SeatDetails>();
+            if (data != null && data.Count > 0)
+            {
+                foreach (var item in data)
+                {
+                    result.AddRange(JsonConvert.DeserializeObject<List<SeatDetails>>(item));
+                }
+            }
+
+            return result;
+
         }
         public  ClientMessage<BookingModel> GetBookingDetails(string bookingnumber,string email)
         {
@@ -57,6 +71,7 @@ namespace BookingSystemApi.ViewModels
            var data = _bookingRepo.Find(bookingnumber);
            var _clientMessage= new ClientMessage<BookingModel>();
            _clientMessage.ClientData=data.Result;
+           //EmaiilHelper
            if(data.IsCompletedSuccessfully)
            {
                _clientMessage.HasError=true;
@@ -65,7 +80,31 @@ namespace BookingSystemApi.ViewModels
            return   _clientMessage;
         }
         #region  Private Methods
-        
+        private void SendMailToTicketOwner(BookingModel model)
+        {
+            string subject= string.Format("Bus ticket confirmation");
+            string message= string.Format("Ticket is booked and booking number is {0}",model.BookingNumber);
+            EmailHelper.SendMail(model.Email,message,subject);
+        }
+        private bool ValidateBooking(BookingModel model)
+        {
+            bool iresult=false;
+            var data = this._bookingRepo.GetBookedTicketDetails(model).Result;
+             var result = new List<SeatDetails>();
+            if (data != null && data.Count > 0)
+            {
+                foreach (var item in data)
+                {
+                    result.AddRange(JsonConvert.DeserializeObject<List<SeatDetails>>(item));
+                }
+            }
+            var dataExist= model.BookedSeatDetails.Except(result);
+            if(dataExist.Count()==model.BookedSeatDetails.Count)
+            {
+                iresult=true;
+            }
+            return iresult;
+        }
         #endregion
     }
 }
